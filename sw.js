0ddf9b5f55f9bd6a7a526a962c0523beee1bee44
yooni536 <yooni536@naver.com>
2026-08-21 20:04:51 +0900

    건강 브리핑 자동 배포 2026-08-21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='health-brief-84f0c3b2';
+const CACHE='health-brief-e6ffb561';
 const SHELL=['./','./index.html','./archive.html','./manifest.webmanifest',
              './icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{
